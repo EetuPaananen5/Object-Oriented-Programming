ea9afe067f7cfd7adbc98c04517183348f854348
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2023-01-10 11:37:27 +0200

    Tässä Book tehtävä

---
+++ BookExercise/Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExercise
+{
+    internal class Book
+    {
+
+        string title;
+        string author;
+        string id;
+        double price;
+        int pageCount;
+  
+        public Book()
+        {
+            this.title = "";
+            this.author = "";
+            this.id = "00000";
+            this.price = 19.90;
+            this.pageCount = 0;
+            this.MyMethod(this.pageCount, this.title);
+
+
+        }
+
+        public Book(string title, int pageCount)
+        {
+            this.title = title;
+            this.pageCount = 0;
+
+            this.author = "";
+            this.id = "00000";
+            this.price = 19.90;
+
+            this.MyMethod(this.pageCount, this.title);
+        }
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.SetId(id);
+            this.price = price;
+
+            this.MyMethod2(this.title, this.author, this.id, this.price);
+        }
+
+   
+
+        public void MyMethod2(string author, string kirjailia, string idee, double parameterPrice)
+        {
+            Console.WriteLine( "this is " + author + " author is " + kirjailia + " id is " + idee + " price is " + parameterPrice + " dollars");
+        }
+
+        public void SetId(string id)
+        {
+            if(id.Length != 5)
+            {
+                Console.WriteLine("invalid id");
+                this.id = "Error";
+            }
+            else
+            {
+                this.id=id;
+            }
+            
+        }
+
+        public void MyMethod(int parameter1, string parameter2)
+        {
+            Console.WriteLine("page count is {0}, " +
+            "title is {1}",
+            parameter1, parameter2);
+        }
+
+     
+
+
+    }
+}
--- BookExercise/Program.cs
+++ BookExercise/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BookExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        Book myFirstBook = new Book();
+        Book mySecondBook = new Book("Peppi Pitkätossu", 77);
+        Book myThirdBook = new Book("HarryPotter", 22);
+        Book myBook = new Book("Muumit", "tove janson", "12345", 15.00);
+        Book myBook2 = new Book("Muumit", "tove janson", "123456", 15.00);
+
+
+    }
+    
+}

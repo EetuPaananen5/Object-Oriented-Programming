2fe93445d7ccc6db250fcb5b67ee910457ec7621
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2023-01-17 14:23:08 +0200

    Company tehtävä tehtynä

---
+++ CompanyExercise/Company.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyExercise
+{
+    internal class Company
+    {
+        string title;
+        string address;
+        string phone;
+        int outcome;
+        int expense;
+
+
+        public Company()
+        {
+            title = string.Empty;
+            address = string.Empty;
+            phone = string.Empty;
+            outcome = 0;
+            expense = 0;
+
+        }
+
+        public Company(Company kopio) //tässä on kopio funktio
+        {
+            this.title = kopio.title;
+            this.address = kopio.address;
+            this.phone = kopio.phone;
+            this.outcome = kopio.outcome;
+            this.expense = kopio.expense;
+        }
+
+
+        public Company(string title, string address, string phone, int outcome, int expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+
+        public void VoittoFunktio ()
+        {
+           
+
+           int tulos = (this.outcome-this.expense)/ this.expense * 100;
+
+            if (tulos < 100)
+            {
+                Console.WriteLine(" Kehnoa tulosta koska voitto on" + tulos);
+            }
+            else if (tulos >= 100 && tulos < 200)
+            {
+                Console.WriteLine(" Välttävä tulos , voitto on " + tulos);
+
+            }
+            else if (tulos >= 200 && tulos < 300)
+            {
+                Console.WriteLine(" tyydyttävä , voitto on " + tulos);
+
+            }
+            else
+            {
+                Console.WriteLine(" hyvä voitto on " + tulos);
+
+            }
+
+
+        }
+
+        public void setTitle(string value)
+        {
+            this.title = value;
+        }
+
+        public void Info(string title, string phone, string address, int outcome, int expense)
+        {
+            this.title = title;
+            this.phone = phone;
+            this.address = address;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+      
+        public void PrintInfo()
+        {
+            Console.WriteLine("Yrityksen nimi:" + this.title );
+            
+        }
+
+        public override string? ToString()
+        {
+            return " nimi "+ this.title + " puhelin " + this.phone + " osoite " + this.address +" tulot "+ this.expense +" menot "+this.outcome;
+        }
+    }
+}
---
+++ CompanyExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompanyExercise;
+
+Console.WriteLine("This is company exercise");
+
+Company finnair = new Company("Finnair Oyj", "Isoroba 1 Helsinki", "045100100", 800000, 200000);
+finnair.setTitle("Finnair Oyj");
+finnair.PrintInfo();
+Console.WriteLine (finnair.ToString());
+finnair.VoittoFunktio();
+
+Console.WriteLine (" ");
+
+Company kamux = new Company("Kamux oy", "Mannerheimintie 5 Helsinki", "045100100", 100000, 50000);
+Company saka = new Company(kamux);
+
+
+Console.WriteLine(kamux.ToString());
+Console.WriteLine(saka.ToString());
+kamux.VoittoFunktio();

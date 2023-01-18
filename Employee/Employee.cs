4567d62f2f85cc5d4cf7e7654ef0734991984220
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2023-01-18 14:32:34 +0200

    Employee tehtävä

---
+++ Employee/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    internal class Employee
+    {
+
+        string name;
+        string id;
+        string position;
+        double salary;
+
+        public Employee(string name, string id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public void CompareSalary(Employee employee)
+        {
+            if (this.salary > employee.salary)
+            {
+                Console.WriteLine( "henkilön "+ this.name + " palkka on suurempi kuin henkilön" + employee.name);
+            }
+            else if (this.salary < employee.salary)
+            {
+                Console.WriteLine("henkilön " + this.name + "palkka on pienempi kuin henkilön" + employee.name);
+            }
+            //else if (this.salary == employee.salary)
+            //{
+              //  Console.WriteLine("henkilön " + this.name + "palkka on yhtä suuri kuin henkilön" + employee.name);
+            //}
+
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine("PrintInfo Nimi: " + this.name +" ID: "+ this.id + " palkka: " + this.salary + " positio: " + this.position);
+
+        }
+
+        public override string? ToString()
+        {
+            return "ToString Nimi:" + this.name + " id: " + this.id + " palkka: " + this.salary + " positio: " + this.position;
+        }
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EmployeeExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("MOikka, tervetuloa konsoliin, alla näkyy palkkavertailu looppi");
+        Console.WriteLine(" ");
+
+        //Employee Eetu = new Employee ("Eetu", "001", "Johtaja", 100000);
+        //Console.WriteLine(Eetu.ToString());
+        //Eetu.PrintEmployeeInfo();
+
+
+        Employee[] employees = new Employee[4];
+        employees[0] = new Employee(" Eetu ", "001", "Johtaja", 100);
+        employees[1] = new Employee(" Sami ", "002", "Myyjä", 200);
+        employees[2] = new Employee(" Juho ", "003", "Siivooja", 300);
+        employees[3] = new Employee(" Dennis ", "004", "Korjaaja", 400);
+
+        for (int i = 0; i < employees.Length; i++)
+        {
+            if(i == employees.Length -1) // jos loopi on lopussa niin vertaa ekaan.
+            {
+                employees[i].CompareSalary(employees[0]);
+            }
+            else
+            {
+                employees[i].CompareSalary(employees[i+1]); //Looppi vertaa seuraavaan
+            }
+           
+        }
+
+        Console.WriteLine(" ");
+
+        for (int i = 0; i < employees.Length; i++)
+        {
+
+            employees[i].PrintEmployeeInfo();
+         
+            Console.WriteLine(employees[i].ToString());
+        }
+
+
+
+
+    }
+}

aff479751adfca2831a9b25586631b751c5fa520
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2023-01-20 12:56:29 +0200

    Pientä hifistelyä konsoli tulostukseen

--- Employee/Employee.cs
+++ Employee/Employee.cs
@@ -22,32 +22,29 @@ namespace EmployeeExercise
             this.salary = salary;
         }
 
-        public void CompareSalary(Employee employee)
+        public void CompareSalary(Employee employee) // if else funktio rakenne joka vertaa palkkoja 
         {
             if (this.salary > employee.salary)
             {
-                Console.WriteLine( "henkilön "+ this.name + " palkka on suurempi kuin henkilön" + employee.name);
+                Console.WriteLine("Henkilön " + this.name + " palkka on suurempi kuin henkilön" + employee.name);
             }
             else if (this.salary < employee.salary)
             {
-                Console.WriteLine("henkilön " + this.name + "palkka on pienempi kuin henkilön" + employee.name);
+                Console.WriteLine("Henkilön " + this.name + "palkka on pienempi kuin henkilön" + employee.name);
             }
-            //else if (this.salary == employee.salary)
-            //{
-              //  Console.WriteLine("henkilön " + this.name + "palkka on yhtä suuri kuin henkilön" + employee.name);
-            //}
+
 
         }
 
         public void PrintEmployeeInfo()
         {
-            Console.WriteLine("PrintInfo Nimi: " + this.name + "\n " +" ID: " + this.id + "\n " + " palkka: " + this.salary + "\n " + " positio: " + this.position);
+            Console.WriteLine("PrintInfo Nimi: " + this.name + "\n " + " ID: " + this.id + "\n " + " palkka: " + this.salary + "\n " + " positio: " + this.position);
 
         }
 
         public override string? ToString()
         {
-            return "\n "+ "ToString Nimi:" + this.name + "\n " + " id: " + this.id + "\n " + " palkka: " + this.salary + "\n " + " positio: " + this.position + "\n ";
+            return "\n " + "ToString Nimi:" + this.name + "\n " + " id: " + this.id + "\n " + " palkka: " + this.salary + "\n " + " positio: " + this.position + "\n ";
         }
     }
 }
--- Employee/Program.cs
+++ Employee/Program.cs
@@ -10,9 +10,6 @@ internal class Program
         Console.WriteLine("Moikka, tervetuloa konsoliin, alla näkyy palkkavertailu looppi");
         Console.WriteLine(" ");
 
-        //Employee Eetu = new Employee ("Eetu", "001", "Johtaja", 100000);
-        //Console.WriteLine(Eetu.ToString());
-        //Eetu.PrintEmployeeInfo();
 
 
         Employee[] employees = new Employee[4];
@@ -21,26 +18,28 @@ internal class Program
         employees[2] = new Employee(" Juho ", "003", "Siivooja", 300);
         employees[3] = new Employee(" Dennis ", "004", "Korjaaja", 400);
 
-        for (int i = 0; i < employees.Length; i++)
+        for (int i = 0; i < employees.Length; i++) // Tässä looppi joka vertaa aina seuraavaan
         {
-            if(i == employees.Length -1) // jos loopi on lopussa niin vertaa ekaan.
+            if (i == employees.Length - 1) // jos loopi on lopussa niin vertaa ekaan.
             {
                 employees[i].CompareSalary(employees[0]);
             }
             else
             {
-                employees[i].CompareSalary(employees[i+1]); //Looppi vertaa seuraavaan
+                employees[i].CompareSalary(employees[i + 1]); //Looppi vertaa seuraavaan
             }
-           
+
         }
 
         Console.WriteLine(" ");
+        Console.WriteLine("Ja tässä alla tulostettuna työntekijöiden tiedot PrintEmployeeInfo funktiona sekä ToString funktiona");
+        Console.WriteLine(" ");
 
-        for (int i = 0; i < employees.Length; i++)
+        for (int i = 0; i < employees.Length; i++) // Tämä looppi tulostaa konsoliin työntekijöiden tiedot
         {
 
             employees[i].PrintEmployeeInfo();
-         
+
             Console.WriteLine(employees[i].ToString());
         }
 

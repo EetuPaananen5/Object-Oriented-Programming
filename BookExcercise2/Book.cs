715c9d58989692582ca7b6ba6399e3bb6b88ca9e
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2023-01-25 11:51:28 +0200

    book2 tehty

---
+++ BookExcercise2/Book.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookExcercise2
+{
+    internal class Book
+    {
+        private string name;
+        private readonly string author;
+        private string publisher;
+        private double price;
+        private string isbn;
+        private static string theme = "Draama";
+        private const int MaxLenght = 13;
+
+        public double Price
+        {
+            get => price;
+            set
+            {
+                if (value > 30)
+
+                { 
+                   price = value * 0.9;
+                }
+
+                else
+                {
+                    price = value;
+                }
+
+            }
+        }
+
+        public string Isbn
+        {
+            get => isbn;
+            set
+            {
+                if(value.Length == MaxLenght && value.StartsWith("123"))
+                {
+                    isbn= value;
+                }
+                else 
+                {
+                    isbn = "invalid inbd";
+                }
+
+
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+                Console.WriteLine(this.author);
+            }
+        }
+
+        public Book(string name, string author, string publisher, double price, string isbn)
+        {
+            this.name = name;
+            this.author = author;
+            this.publisher = publisher;
+            this.Price = price;
+            this.Isbn = isbn;
+        }
+
+        public void GetBookDetails(string tunnus)
+        {
+
+            if (tunnus == Isbn)
+            {
+                Console.WriteLine("Tässä on kirjan tiedot" + "\n " + this.name + "\n " + this.Author + "\n " + this.publisher + "\n " + this.Price.ToString("0.00") + "\n " + "Teema on" + "\n " + theme);
+            }
+            else
+            {
+                Console.WriteLine("Tämän kirjan ISBN tunnus ei täsmää");
+            }
+
+        }
+
+        public static void ChangeTheme(string value)
+        {
+            theme = value;
+        }
+    }
+
+
+
+
+
+
+}
---
+++ BookExcercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BookExcercise2;
+using System.Runtime.CompilerServices;
+
+Book kirja1 = new Book("Aapinen", "Maija Mehiläinen", "Otava", 15.50 , "1234567891123") ;
+
+Book kirja2 = new Book("Yön ritari", "Ilkka Remes", "WSOY", 40.00, "1234567891123");
+
+
+kirja1.GetBookDetails("1234567891123");
+
+Console.WriteLine(" ");
+kirja2.GetBookDetails("1234567891123");
+
+Console.WriteLine(" ");
+
+Book.ChangeTheme("komedia");
+kirja1.GetBookDetails("1234567891123");
+Console.WriteLine(" ");
+kirja2.GetBookDetails("1234567891123");
+
+Console.WriteLine(kirja1.Author);
+
+
+
+
